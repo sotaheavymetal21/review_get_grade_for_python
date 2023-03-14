74d92977c3371496e05b72e476e970f8f11ad910
sotaheavymetal21 <euro246seeker@gmail.com>
2023-03-14 22:56:54 +0900

    上位の評価とあと何点かを出力

--- application/template/example.py
+++ application/template/example.py
@@ -3,34 +3,43 @@
 """例題プログラム"""
 
 
-def get_grade(score):
+def get_grade(score: int) -> str:
     grade_dict = {
         "S+": 100,
-        "A+": 95,
-        "A": 90,
-        "B+": 85,
-        "B": 80,
-        "C+": 75,
-        "C": 70,
-        "D+": 65,
-        "D": 60,
-        "F+": 55,
+        "S": 95,
+        "A+": 90,
+        "A": 85,
+        "B+": 80,
+        "B": 75,
+        "C+": 70,
+        "C": 65,
+        "D+": 60,
+        "D": 55,
         "F": 0,
     }
 
     if score < 0 or score > 100:
-        return "Score is out of range"
+        return "存在しない点数です。"
 
-    for key in sorted(grade_dict.keys(), reverse=True):
-        if score >= grade_dict[key]:
-            return key
+    for i, (grade, min_score) in enumerate(
+        sorted(grade_dict.items(), key=lambda x: -x[1])
+    ):
+        if score >= min_score:
+            if i == 0:
+                return "最高評価です！"
+            else:
+                prev_grade, prev_min_score = sorted(
+                    grade_dict.items(), key=lambda x: -x[1]
+                )[i - 1]
+                return f"{grade}まであと {min_score-score}点です。次の上位は{prev_grade}で、あと{score-prev_min_score}点必要です。"
 
-    return "Score is out of range"
+    return "最低評価です。。。"
 
 
-print(get_grade(99))
-print(get_grade(85))
-print(get_grade(30))
-print(get_grade(100))
-print(get_grade(110))
-print(get_grade(-10))
+print(get_grade(82))  # Aまであと 3点です。次の上位はA+で、あと 8点必要です。
+print(get_grade(33))  # Cまであと 2点です。次の上位はC+で、あと 2点必要です。
+print(get_grade(2))  # Fまであと 53点です。次の上位はDで、あと 53点必要です。
+print(get_grade(0))  # Fまであと 55点です。次の上位はDで、あと 55点必要です。
+print(get_grade(33.5))  # Cまであと 1.5点です。次の上位はC+で、あと 1.5点必要です。
+print(get_grade(100))  # 最高評価です！
+print(get_grade(-10))  # 存在しない点数です。
