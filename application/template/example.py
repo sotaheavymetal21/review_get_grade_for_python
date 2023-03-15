541e0e98459146e799c539980e5d0412db2e290a
sotaheavymetal21 <euro246seeker@gmail.com>
2023-03-15 23:13:27 +0900

    テスト修正

--- application/template/example.py
+++ application/template/example.py
@@ -3,32 +3,34 @@
 """例題プログラム"""
 
 
-def next_grade_score(score):
-    grade_dict = {
-        "S+": 100,
-        "S": 95,
-        "A+": 90,
-        "A": 85,
-        "B+": 80,
-        "B": 75,
-        "C+": 70,
-        "C": 65,
-        "D+": 60,
-        "D": 55,
-        "F": 0,
-    }
+grade_dict = {
+    "S+": 100,
+    "S": 95,
+    "A+": 90,
+    "A": 85,
+    "B+": 80,
+    "B": 75,
+    "C+": 70,
+    "C": 65,
+    "D+": 60,
+    "D": 55,
+    "F": 0,
+}
+
+
+def get_score(score_of_test):
     not_exist = "存在しない点数です。"
-    if score == 100:
+    if score_of_test is 100:
         return "満点です！素晴らしい！"
-    if not isinstance(score, int):
+    if not isinstance(score_of_test, int):
         return "存在しない点数です。"
-    if score < 0:
+    if score_of_test < 0:
         return not_exist
-    if score > 100:
+    if score_of_test > 100:
         return not_exist
-    grade_list = []
+    list = []
     for grade in grade_dict.keys():
-        if score >= grade_dict[grade]:
-            return f"現在の評価は{grade}です。上位の評価({grade_list[-1]})まであと{grade_dict[grade_list[-1]] - score}点です。"
+        if score_of_test >= grade_dict[grade]:
+            return f"現在の評価は{grade}です。上位の評価({list[-1]})まであと{grade_dict[list[-1]] - score_of_test}点です。"
         else:
-            grade_list.append(grade)
+            list.append(grade)
--- application/tests/test_example.py
+++ application/tests/test_example.py
@@ -3,39 +3,39 @@
 """例題プログラム"""
 import pytest
 
-from template.example import next_grade_score
+from template.example import get_score
 
 
 def test_S():
-    """グレードが S のとき"""
-    assert next_grade_score(95) == "現在の評価はSです。上位の評価(S+)まであと5点です。"
+    """評価が S のとき"""
+    assert get_score(95) == "現在の評価はSです。上位の評価(S+)まであと5点です。"
 
 
 def test_C():
-    """グレードが C+ のとき"""
-    assert next_grade_score(68) == "現在の評価はCです。上位の評価(C+)まであと2点です。"
+    """評価が C+ のとき"""
+    assert get_score(68) == "現在の評価はCです。上位の評価(C+)まであと2点です。"
 
 
 def test_E():
-    """グレードが D のとき"""
-    assert next_grade_score(58) == "現在の評価はDです。上位の評価(D+)まであと2点です。"
+    """評価が D のとき"""
+    assert get_score(58) == "現在の評価はDです。上位の評価(D+)まであと2点です。"
 
 
 def test_F():
     """グレードが F のとき"""
-    assert next_grade_score(4) == "現在の評価はFです。上位の評価(D)まであと51点です。"
+    assert get_score(4) == "現在の評価はFです。上位の評価(D)まであと51点です。"
 
 
 def test_not_exist_point():
     """存在しない点数（100よりも大きい）のとき"""
-    assert next_grade_score(105) == "存在しない点数です。"
+    assert get_score(105) == "存在しない点数です。"
 
 
 def test_not_exist_point2():
     """存在しない点数（マイナス）のとき"""
-    assert next_grade_score(-15) == "存在しない点数です。"
+    assert get_score(-15) == "存在しない点数です。"
 
 
 def test_not_integer():
     """存在しない点数（小数点）のとき"""
-    assert next_grade_score(22.5) == "存在しない点数です。"
+    assert get_score(22.5) == "存在しない点数です。"
