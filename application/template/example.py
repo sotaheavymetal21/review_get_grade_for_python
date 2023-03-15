cd13508f86e2cd01fa16a43c0f2066569b4a15c4
sotaheavymetal21 <euro246seeker@gmail.com>
2023-03-15 22:05:19 +0900

    出力例を修正

--- application/template/example.py
+++ application/template/example.py
@@ -29,16 +29,10 @@ def next_grade_score(score):
             grade_list.append(grade)
 
 
-print(next_grade_score(99))  # 上位の評価S+まであと1点です。
-print(next_grade_score(82))  # 上位の評価Aまであと3点です。
-print(next_grade_score(71))  # 上位の評価Bまであと4点です。
-print(next_grade_score(66))  # 上位の評価C+まであと4点です。
+print(next_grade_score(99))  # 現在の評価はSです。上位の評価(S+)まであと1点です。
+print(next_grade_score(82))  # 現在の評価はB+です。上位の評価(A)まであと3点です。
+print(next_grade_score(71))  # 現在の評価はC+です。上位の評価(B)まであと4点です。
+print(next_grade_score(66))  # 現在の評価はCです。上位の評価(C+)まであと4点です。
 print(next_grade_score(-15))  # 存在しない点数です。
-print(next_grade_score(30.5))  # 上位の評価Dまであと24.5点です。
-print(next_grade_score(100))  # 上位の評価S+まであと0点です。
-
-#    for grade in grade_dict.keys():
-#       if score > grade_dict[grade]:
-#          return f"現在の評価は{grade}です。上位の評価({grade_list[-1]})まであと{grade_dict[grade_list[-1]] - score}点です。"
-#     else:
-#        grade_list.append(grade)
+print(next_grade_score(30.5))  # 存在しない点数です。
+print(next_grade_score(100))  # 満点です！素晴らしい！
